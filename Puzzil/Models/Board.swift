9f61c9dfdfbcad15606aee21c7362e2c7f81a759
Rizadh Nizam <me@rizadh.com>
2018-05-06 23:25:49 -0400

    Update documentation for Board

--- Puzzil/Models/Board.swift
+++ Puzzil/Models/Board.swift
@@ -9,10 +9,15 @@
 import Foundation
 
 struct Board {
+    private typealias TileMatrix = [[Tile?]]
+
+    // MARK: - Board Properties
+
     let rows: Int
     let columns: Int
-    private let originalTiles: [[Tile?]]
-    private var tiles = [[Tile?]]()
+    private var tiles: TileMatrix = []
+
+    // MARK: - Board Status
 
     var isSolved: Bool {
         for position in TilePosition.traversePositions(rows: rows, columns: columns) {
@@ -24,19 +29,25 @@ struct Board {
         return true
     }
 
+    var progress: Double {
+        return 1 - Double(distanceLeft) / Double(maxDistanceRemaining)
+    }
+
     private var distanceLeft: Int {
         var distance = 0
 
         for position in TilePosition.traversePositions(rows: rows, columns: columns) {
             if let tile = tiles[position], !tile.targets.contains(position) {
-                distance += tile.targets.map(position.distance).min()!
+                distance += tile.targets.map(position.distance(to:)).min()!
             }
         }
 
         return distance
     }
 
-    private var maxDistanceRemaining: Int {
+    private let maxDistanceRemaining: Int
+
+    private static func calculateMaxDistance(for tiles: TileMatrix, rows: Int, columns: Int) -> Int {
         var maxDistance = 0
 
         let topLeftCorner = TilePosition(row: 0, column: 0)
@@ -45,7 +56,7 @@ struct Board {
         let bottomLeftCorner = TilePosition(row: rows - 1, column: 0)
 
         for position in TilePosition.traversePositions(rows: rows, columns: columns) {
-            if originalTiles[position] != nil {
+            if tiles[position] != nil {
                 maxDistance += max(
                     position.distance(to: topLeftCorner),
                     position.distance(to: topRightCorner),
@@ -58,9 +69,7 @@ struct Board {
         return maxDistance
     }
 
-    var progress: Double {
-        return 1 - Double(distanceLeft) / Double(maxDistanceRemaining)
-    }
+    // MARK: - Constructors
 
     init(from matrix: [[CustomStringConvertible?]]) {
         guard matrix.count > 0 else { fatalError("Matrix must have at least one row") }
@@ -105,19 +114,23 @@ struct Board {
             return Tile(targets: positions, text: text)
         } }
 
-        originalTiles = tiles
+        maxDistanceRemaining = Board.calculateMaxDistance(for: tiles, rows: rows, columns: columns)
     }
 
-    func boardContains(_ position: TilePosition) -> Bool {
+    // MARK: - Private Methods
+
+    private func boardContains(_ position: TilePosition) -> Bool {
         return position.row >= 0 && position.row < rows &&
             position.column >= 0 && position.column < columns
     }
 
-    func tileIsPresent(at position: TilePosition) -> Bool? {
+    private func tileIsPresent(at position: TilePosition) -> Bool? {
         guard boardContains(position) else { return nil }
 
         return tiles[position] != nil
     }
+
+    // MARK: - Public Methods
 
     func canPerform(_ moveOperation: TileMoveOperation) -> Bool? {
         let target = moveOperation.targetPosition
@@ -159,7 +172,8 @@ struct Board {
     }
 }
 
-extension Array where Element == Array<Tile?> {
+// Facilitate retrieving tiles from a TileMatrix
+extension Array where Element == [Tile?] {
     subscript(_ position: TilePosition) -> Tile? {
         get {
             return self[position.row][position.column]
