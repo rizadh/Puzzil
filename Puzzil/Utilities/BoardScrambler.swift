35e1820757d8cb76be47a03d23e741d2e15bd5c5
Rizadh Nizam <me@rizadh.com>
2018-05-31 17:50:01 -0400

    Shorten scrambled board queue

--- Puzzil/Utilities/BoardScrambler.swift
+++ Puzzil/Utilities/BoardScrambler.swift
@@ -13,7 +13,7 @@ class BoardScrambler {
 
     init() {
         boardGenerators = Dictionary(uniqueKeysWithValues: BoardStyle.all.map { boardStyle in
-            let generator = QueuedGenerator(name: boardStyle.rawValue, queueLength: 5) {
+            let generator = QueuedGenerator(name: boardStyle.rawValue, queueLength: 2) {
                 return BoardScrambler.generateBoard(style: boardStyle)
             }
 
