1e54a892c6b20e40663331f10fc8ff37e5051359
Rizadh Nizam <me@rizadh.com>
2018-08-27 13:09:10 -0400

    Add a disabled state to ThemedButton

--- Puzzil/Views/ThemedButton.swift
+++ Puzzil/Views/ThemedButton.swift
@@ -11,38 +11,62 @@ import UIKit
 class ThemedButton: UIButton {
     private let highlightLayer = CALayer()
 
+    override var isHighlighted: Bool {
+        didSet {
+            isHighlighted ? activateHighlight() : deactivateHighlight()
+        }
+    }
+
+    override var isEnabled: Bool {
+        didSet {
+            isEnabled ? setEnabledTintColor() : setDisabledTintColor()
+        }
+    }
+
     convenience init() {
         self.init(type: .custom)
 
         layer.cornerRadius = 16
         highlightLayer.cornerRadius = 16
         highlightLayer.opacity = 0
+        let baseColor: UIColor
         switch ColorTheme.selected {
         case .light:
-            highlightLayer.backgroundColor = UIColor.black.withAlphaComponent(0.2).cgColor
+            baseColor = .black
         case .dark:
-            highlightLayer.backgroundColor = UIColor.white.withAlphaComponent(0.2).cgColor
+            baseColor = .white
         }
+        highlightLayer.backgroundColor = baseColor.withAlphaComponent(0.2).cgColor
         layer.addSublayer(highlightLayer)
         backgroundColor = ColorTheme.selected.primary
-        tintColor = ColorTheme.selected.primaryTextOnPrimary
+        setEnabledTintColor()
         setTitleColor(ColorTheme.selected.primaryTextOnPrimary, for: .normal)
+        setTitleColor(ColorTheme.selected.primaryTextOnPrimary.withAlphaComponent(0.5), for: .disabled)
         titleLabel!.font = UIFont.systemFont(ofSize: 24, weight: .bold)
         adjustsImageWhenHighlighted = false
         adjustsImageWhenDisabled = false
-
-        addTarget(self, action: #selector(buttonWasPressed), for: [.touchDown, .touchDragEnter])
-        addTarget(self, action: #selector(buttonWasReleased), for: [.touchUpInside, .touchCancel, .touchDragExit])
     }
 
-    @objc private func buttonWasPressed() {
+    private func setEnabledTintColor() {
+        UIViewPropertyAnimator(duration: 0.1, curve: .linear) {
+            self.tintColor = ColorTheme.selected.primaryTextOnPrimary
+        }.startAnimation()
+    }
+
+    private func setDisabledTintColor() {
+        UIViewPropertyAnimator(duration: 0.1, curve: .linear) {
+            self.tintColor = ColorTheme.selected.primaryTextOnPrimary.withAlphaComponent(0.5)
+        }.startAnimation()
+    }
+
+    private func activateHighlight() {
         CATransaction.begin()
         CATransaction.setDisableActions(true)
         highlightLayer.opacity = 1
         CATransaction.commit()
     }
 
-    @objc private func buttonWasReleased() {
+    private func deactivateHighlight() {
         CATransaction.begin()
         CATransaction.setAnimationDuration(0.25)
         CATransaction.setAnimationTimingFunction(CAMediaTimingFunction(name: CAMediaTimingFunctionName.linear))
